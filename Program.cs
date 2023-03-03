cd8a121e0d8957a9037e16a29cd34037c3d5916f
Jens Gustafsson <118354433+Wavephoria@users.noreply.github.com>
2023-03-03 16:45:27 +0100

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-var app = builder.Build();
-
-app.UseRouting();
-// app.MapGet("/", () => "Hello World!!!");
-app.UseEndpoints(o => o.MapControllers());
-
-app.Run();
