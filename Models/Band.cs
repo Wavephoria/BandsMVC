5a6c3cfdf1df816d320ff8e8311a20b8d68b5c26
Jens Gustafsson <118354433+Wavephoria@users.noreply.github.com>
2023-03-03 16:44:53 +0100

    Delete Models directory

--- Models/Band.cs
+++
@@ -1,9 +0,0 @@
-namespace BandsMVC.Models
-{
-    public class Band
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
